e57466e27f8d8922d83b4f0fbe114d802af33b77
Crazy_Phage <ufozhengli@163.com>
2014-03-23 15:35:30 +0800

    commit to save
    fix some bugs

--- Awesomepresentation/public/js/demo.js
+++ Awesomepresentation/public/js/demo.js
@@ -84,259 +84,7 @@ $(function() {
 
     });
 
-    // Todo Item View
-    // --------------
 
-    // The DOM element for a todo item...
-    var TodoView = AV.View.extend({
-
-        //... is a list tag.
-        tagName: "li",
-
-        // Cache the template function for a single item.
-        template: _.template($('#item-template').html()),
-
-        // The DOM events specific to an item.
-        events: {
-            "click .toggle": "toggleDone",
-            "dblclick label.todo-content": "edit",
-            "click .todo-destroy": "clear",
-            "click .upd": "updateOnEnter"
-            // ,
-            // "blur .edit": "close"
-        },
-
-        // The TodoView listens for changes to its model, re-rendering. Since there's
-        // a one-to-one correspondence between a Todo and a TodoView in this
-        // app, we set a direct reference on the model for convenience.
-        initialize: function() {
-            _.bindAll(this, 'render', 'close', 'remove');
-            this.model.bind('change', this.render);
-            this.model.bind('destroy', this.remove);
-        },
-
-        // Re-render the contents of the todo item.
-        render: function() {
-            $(this.el).html(this.template(this.model.toJSON()));
-
-             this.$el.html(this.template(this.model.toJSON()));
-            return this;
-        },
-
-        // Toggle the `"done"` state of the model.
-        toggleDone: function() {
-            this.model.toggle();
-        },
-
-        // Switch this view into `"editing"` mode, displaying the input field.
-        edit: function() {
-            $(this.el).addClass("editing");
-            this.input.focus();
-        },
-
-        // Close the `"editing"` mode, saving changes to the todo.
-        close: function() {
-            this.model.save({
-                  content: $("input[name='content']").val(),
-                  step:$("input[name='step']").val(),
-                  dataX:$("input[name='dataX']").val(),
-                  dataY:$("input[name='dataY']").val(),
-                  dataZ:$("input[name='dataZ']").val(),
-                  dataRotateX:$("input[name='dataRotateX']").val(),
-                  dataRotateY:$("input[name='dataRotateY']").val(),
-                  dataRotateZ:$("input[name='dataRotateZ']").val(),
-                  dataScale:$("input[name='dataScale']").val()
-                //todo add other vals
-                
-
-            });
-            $(this.el).removeClass("editing");
-        },
-
-        // If you hit `enter`, we're through editing the item.
-        updateOnEnter: function(e) {
-            this.close();
-        },
-
-        // Remove the item, destroy the model.
-        clear: function() {
-            this.model.destroy();
-        }
-
-    });
-
-    // The Application
-    // ---------------
-
-    // The main view that lets a user manage their todo items
-    var ManageTodosView = AV.View.extend({
-
-        // Our template for the line of statistics at the bottom of the app.
-        statsTemplate: _.template($('#stats-template').html()),
-
-        // Delegated events for creating new items, and clearing completed ones.
-        events: {
-            "click .save": "createOnEnter",
-            "click #clear-completed": "clearCompleted",
-            "click #toggle-all": "toggleAllComplete",
-            "click .log-out": "logOut",
-            "click ul#filters a": "selectFilter"
-        },
-
-        el: ".content",
-
-        // At initialization we bind to the relevant events on the `Todos`
-        // collection, when items are added or changed. Kick things off by
-        // loading any preexisting todos that might be saved to AV.
-        initialize: function() {
-            var self = this;
-
-            _.bindAll(this, 'addOne', 'addAll', 'addSome', 'render', 'toggleAllComplete', 'logOut', 'createOnEnter');
-
-            // Main todo management template
-            this.$el.html(_.template($("#manage-todos-template").html()));
-
-            this.input = this.$("#new-todo");
-            this.allCheckbox = this.$("#toggle-all")[0];
-
-            // Create our collection of Todos
-            this.todos = new TodoList;
-
-            // Setup the query for the collection to look for todos from the current user
-            this.todos.query = new AV.Query(Todo);
-            this.todos.query.equalTo("user", AV.User.current());
-
-            this.todos.bind('add', this.addOne);
-            this.todos.bind('reset', this.addAll);
-            this.todos.bind('all', this.render);
-
-            // Fetch all the todo items for this user
-            this.todos.fetch();
-
-            state.on("change", this.filter, this);
-        },
-
-        // Logs out the user and shows the login view
-        logOut: function(e) {
-            AV.User.logOut();
-            new LogInView();
-            this.undelegateEvents();
-            delete this;
-        },
-
-        // Re-rendering the App just means refreshing the statistics -- the rest
-        // of the app doesn't change.
-        render: function() {
-            var done = this.todos.done().length;
-            var remaining = this.todos.remaining().length;
-
-            this.$('#todo-stats').html(this.statsTemplate({
-                total: this.todos.length,
-                done: done,
-                remaining: remaining
-            }));
-
-            this.delegateEvents();
-
-            this.allCheckbox.checked = !remaining;
-        },
-
-        // Filters the list based on which type of filter is selected
-        selectFilter: function(e) {
-            var el = $(e.target);
-            var filterValue = el.attr("id");
-            state.set({
-                filter: filterValue
-            });
-            AV.history.navigate(filterValue);
-        },
-
-        filter: function() {
-            var filterValue = state.get("filter");
-            this.$("ul#filters a").removeClass("selected");
-            this.$("ul#filters a#" + filterValue).addClass("selected");
-            if (filterValue === "all") {
-                this.addAll();
-            } else if (filterValue === "completed") {
-                this.addSome(function(item) {
-                    return item.get('done')
-                });
-            } else {
-                this.addSome(function(item) {
-                    return !item.get('done')
-                });
-            }
-        },
-
-        // Resets the filters to display all todos
-        resetFilters: function() {
-            this.$("ul#filters a").removeClass("selected");
-            this.$("ul#filters a#all").addClass("selected");
-            this.addAll();
-        },
-
-        // Add a single todo item to the list by creating a view for it, an
-        // appending its element to the `<ul>`.
-        addOne: function(todo) {
-            var view = new TodoView({
-                model: todo
-            });
-            this.$("#todo-list").append(view.render().el);
-        },
-
-        // Add all items in the Todos collection at once.
-        addAll: function(collection, filter) {
-            this.$("#todo-list").html("");
-            this.todos.each(this.addOne);
-        },
-
-        // Only adds some todos, based on a filtering function that is passed in
-        addSome: function(filter) {
-            var self = this;
-            this.$("#todo-list").html("");
-            this.todos.chain().filter(filter).each(function(item) {
-                self.addOne(item)
-            });
-        },
-
-        // If you hit return in the main input field, create new Todo model
-
-
-
-
-        createOnEnter: function(e) {
-            var self = this;
-          
-
-            this.todos.create({
-                content: this.input.val(),
-                order: this.todos.nextOrder(),
-                done: false,
-                user: AV.User.current(),
-                ACL: new AV.ACL(AV.User.current())
-            });
-
-
-            this.resetFilters();
-        },
-
-        // Clear all done todo items, destroying their models.
-        clearCompleted: function() {
-            _.each(this.todos.done(), function(todo) {
-                todo.destroy();
-            });
-            return false;
-        },
-
-        toggleAllComplete: function() {
-            var done = this.allCheckbox.checked;
-            this.todos.each(function(todo) {
-                todo.save({
-                    'done': done
-                });
-            });
-        }
-    });
 
     var LogInView = AV.View.extend({
         events: {
@@ -344,7 +92,7 @@ $(function() {
             "submit form.signup-form": "signUp"
         },
 
-        el: ".content",
+        el: ".body",
 
         initialize: function() {
             _.bindAll(this, "logIn", "signUp");
@@ -402,6 +150,140 @@ $(function() {
             this.delegateEvents();
         }
     });
+ var IuntiView = AV.View.extend({
+
+        //... is a list tag.
+
+
+        // Cache the template function for a single item.
+        template: _.template($('#iunit-template').html()),
+
+        // The DOM events specific to an item.
+        events: {
+
+            // ,
+            // "blur .edit": "close"
+        },
+
+        // The TodoView listens for changes to its model, re-rendering. Since there's
+        // a one-to-one correspondence between a Todo and a TodoView in this
+        // app, we set a direct reference on the model for convenience.
+        initialize: function() {
+            _.bindAll(this, 'render', 'close', 'remove');
+            this.model.bind('change', this.render);
+            this.model.bind('destroy', this.remove);
+        },
+
+        // Re-render the contents of the todo item.
+        render: function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+             console.log(this.template(this.model.toJSON()) );
+            return this;
+        },
+
+        // Close the `"editing"` mode, saving changes to the todo.
+        close: function() {
+            this.model.save({
+                  content: this.input.val(),
+                  step:$("input[name='step']").val(),
+                  dataX:$("input[name='dataX']").val(),
+                  dataY:$("input[name='dataY']").val(),
+                  dataZ:$("input[name='dataZ']").val(),
+                  dataRotateX:$("input[name='dataRotateX']").val(),
+                  dataRotateY:$("input[name='dataRotateY']").val(),
+                  dataRotateZ:$("input[name='dataRotateZ']").val(),
+                  dataScale:$("input[name='dataScale']").val()
+            });
+
+        }
+
+    });
+
+    // The Application
+    // ---------------
+
+    // The main view that lets a user manage their todo items
+    var ImpressDemoView = AV.View.extend({
+
+
+        // Delegated events for creating new items, and clearing completed ones.
+        events: {
+            "click .log-out": "logOut"
+        },
+
+        el: ".todoapp",
+
+        // At initialization we bind to the relevant events on the `Todos`
+        // collection, when items are added or changed. Kick things off by
+        // loading any preexisting todos that might be saved to AV.
+        initialize: function() {
+            var self = this;
+
+            _.bindAll(this, 'addOne', 'addAll', 'addSome',  'logOut');
+
+            // Main todo management template
+            this.$el.html(_.template($("#impress-demo-template").html()));
+
+            this.input = this.$("#new-todo");
+            this.allCheckbox = this.$("#toggle-all")[0];
+
+            // Create our collection of Todos
+            this.todos = new TodoList;
+
+            // Setup the query for the collection to look for todos from the current user
+            this.todos.query = new AV.Query(Todo);
+            this.todos.query.equalTo("user", AV.User.current());
+
+            this.todos.bind('add', this.addOne);
+            this.todos.bind('reset', this.addAll);
+            this.todos.bind('all', this.render);
+
+            // Fetch all the todo items for this user
+            this.todos.fetch();
+
+            state.on("change", this.filter, this);
+        },
+
+        // Logs out the user and shows the login view
+        logOut: function(e) {
+            AV.User.logOut();
+            new LogInView();
+            this.undelegateEvents();
+            delete this;
+        },
+
+
+
+
+
+
+        // Add a single todo item to the list by creating a view for it, an
+        // appending its element to the `<ul>`.
+        addOne: function(todo) {
+            var view = new IuntiView({
+                model: todo
+            });
+            this.$("#impress").append(view.render().el);
+        },
+
+        // Add all items in the Todos collection at once.
+        addAll: function(collection, filter) {
+            this.$("#impress").html("");
+            this.todos.each(this.addOne);
+        },
+
+        // Only adds some todos, based on a filtering function that is passed in
+        addSome: function(filter) {
+            var self = this;
+            this.$("#impress").html("");
+            this.todos.chain().filter(filter).each(function(item) {
+                self.addOne(item)
+            });
+        }
+    });
+
+
+
 
     // The main view for the app
     var AppView = AV.View.extend({
@@ -415,7 +297,7 @@ $(function() {
 
         render: function() {
             if (AV.User.current()) {
-                new ManageTodosView();
+                new ImpressDemoView();
             } else {
                 new LogInView();
             }
